aaf45283bf2f28a7c5d11bc24e7ea78e559a02de
MALAKA-xxxx <sadaruwanmoved@gmail.com>
2024-11-17 08:24:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "please enter your session code 😊😊",
-MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
+SESSION_ID: process.env.SESSION_ID || "oycUxCJY#QzfJwchmQXDhAhxBHKgLwKRLNf-sxtW8MlfOKTNl0sA",
+MONGODB: process.env.MONGODB || "mongodb://mongo:EdhPLwBbfhyOLwmgdkRatAcogBsKbakN@autorack.proxy.rlwy.net:23902",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_5278.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true"
